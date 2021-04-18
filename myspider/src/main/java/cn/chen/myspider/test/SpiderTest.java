e25785fb9dea7f52319866d44307bc8d1edfaa29
bugger <bugger@email.com>
2021-04-18 10:57:00 +0800

    selenium phantomjsdriver 爬取京东首页

--- myspider/src/main/java/cn/chen/myspider/test/SpiderTest.java
+++ myspider/src/main/java/cn/chen/myspider/test/SpiderTest.java
@@ -14,12 +14,19 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class SpiderTest {
 
@@ -117,5 +124,55 @@ public class SpiderTest {
         HttpEntity entity = response.getEntity();
         String out = EntityUtils.toString(entity, "UTF-8");
         System.out.println(out);
+    }
+
+    /*
+        参考:
+        https://blog.csdn.net/wuyinggui10000/article/details/91454504
+     */
+    @Test
+    public void testSel() throws Exception {
+        System.setProperty("webdriver.chrome.driver","D:\\codehub\\downloads\\chromedriver.exe");
+
+        // 首选项
+        Map<String, Object> opts = new HashMap<String, Object>();       // options
+        opts.put("profile.managed_default_content_settings.images", 2); // 禁止加载图片
+        opts.put("profile.default_content_settings.cookies", 2);        // 禁止cookies
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", opts);
+
+        // 新建一个谷歌浏览器对象(diver)
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.manage().window().maximize();
+            /*
+                一个显式等待是你定义的一段代码，用于等待某个条件发生然后再继续执行后续代码。显式等待是等元素加载！！！
+
+                隐式等待: 相当于设置全局的等待，在定位元素时，对所有元素设置超时时间。隐式等待是等页面加载，而不是元素加载！！！
+
+                隐式等待就是针对页面的，显式等待是针对元素的。
+             */
+            WebDriverWait wait = new WebDriverWait(driver, 10); // 显式等待 until
+            // 与浏览器同步非常重要，必须等待浏览器加载完毕
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // 隐式等待
+
+            // 通过driver控制浏览器打开链接
+            String url = "https://www.jd.com/";
+            driver.get(url);
+
+            // 页面标题
+            String title = driver.getTitle();
+            // 页面代码
+            String html = driver.getPageSource();
+            System.out.println(html);
+        }catch (Exception e){
+            e.printStackTrace();
+            driver.close();
+
+//            testSel(); // 666
+        }finally {
+            driver.close();
+        }
     }
 }
