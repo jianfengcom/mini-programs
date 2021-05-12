6618824e38af20703bbd388a08161779890f42e0
bugger <bugger@email.com>
2021-05-12 15:44:54 +0800

    post json参数

--- myspider/src/main/java/cn/chen/myspider/test/pc/Best.java
+++ myspider/src/main/java/cn/chen/myspider/test/pc/Best.java
@@ -1,10 +1,13 @@
 package cn.chen.myspider.test.pc;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
@@ -46,8 +49,44 @@ public class Best {
 
         CloseableHttpResponse response = httpClient.execute(get);
         HttpEntity entity = response.getEntity();
-        String out = EntityUtils.toString(entity, "GBK");
+        String out = EntityUtils.toString(entity, "UTF-8");
 
         System.out.println(out);
     }
+
+    /**
+     * POST请求, json类型参数
+     * 简版
+     */
+    @SuppressWarnings("Duplicates")
+    public void jsonPost(String url, JSONObject params) throws Exception {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost(url);
+
+        // 设置请求配置的代理地址
+        RequestConfig reqConfig = RequestConfig.custom()
+                .setConnectTimeout(3000) // 设置连接超时时间, 单位毫秒
+                .setSocketTimeout(3000) // 设置读取超时时间, 单位毫秒
+                .build();
+
+        // 设置请求配置
+        post.setConfig(reqConfig);
+
+        // 传值时传递的是json字符串，这样的好处是在服务端无需建立参数模型，直接接收String。
+        StringEntity stringEntity = new StringEntity(params.toJSONString(), "UTF-8");
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        HttpEntity entity = response.getEntity();
+        String out = EntityUtils.toString(entity, "UTF-8");
+
+        System.out.println(out);
+    }
+    /*@RequestMapping("/run")
+    @ResponseBody
+    public String run(@RequestBody String jsonStr) {
+        JSONObject jo = JSONObject.parseObject(jsonStr);JSONArray ja = jo.getJSONArray("productIds");for (int i = 0; i < ja.size(); i++) { System.out.println(ja.getLongValue(i)); }
+        return "run";
+    }*/
 }
